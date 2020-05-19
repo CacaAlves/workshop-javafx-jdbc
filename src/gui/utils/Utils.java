c0f484bcf6d64ed7818abe2fa899bce84a068f56
CacaAlves <cacazinho99@outlook.com>
2020-05-19 03:19:13 -0300

    btSave and btCancel

--- src/gui/utils/Utils.java
+++ src/gui/utils/Utils.java
@@ -8,4 +8,14 @@ public class Utils {
 	public static Stage currentStage(ActionEvent event) {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 	}
+	
+	public static Integer tryParseToInt(String str) {
+		try {
+			Integer number = Integer.parseInt(str);
+			return number;
+		} 
+		catch (NumberFormatException e) {
+			return null;
+		}
+	}
 }
